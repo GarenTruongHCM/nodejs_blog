2126daee9580b9c05dd9ec3c66972a0d398434f2
garen <garen@hanbiro.vn>
2021-02-18 16:16:55 +0700

    new update

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ const handlebars = require("express-handlebars");
 const post = 3000;
 
 app.use(morgan(`combined`));
-app.engine("handlebars", handlebars());
-app.set("view engine", "handlebars");
 
+app.engine("hbs", handlebars({ extname: ".hbs" }));
+app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resource/views"));
+
 app.get("/", (req, res) => {
-  res.render("home")
+  res.render("home");
 });
 
 app.listen(post, () => console.log(`app listening http://localhost:${post}`));

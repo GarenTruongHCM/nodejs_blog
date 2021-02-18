447a4ef07efdad7b558a38a09aebc08639669ece
garen <garen@hanbiro.vn>
2021-02-18 21:13:40 +0700

    routing

--- src/index.js
+++ src/index.js
@@ -5,14 +5,22 @@ const morgan = require("morgan");
 const handlebars = require("express-handlebars");
 const post = 3000;
 
+// http logger
 app.use(morgan(`combined`));
 
+app.use(express.static(path.join(__dirname, "public")));
+
+// template engine
 app.engine("hbs", handlebars({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resource/views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
 });
 
 app.listen(post, () => console.log(`app listening http://localhost:${post}`));
